82a568c204eb7308f35320f7651665e6631114c9
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-10-28 12:00:40 +0800

    Login - submit

--- resources/views/new/login.blade.php
+++ resources/views/new/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('New Login Using kaddebit DB') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('new-login:process') }}">
                         @csrf
 
                         <div class="form-group row">

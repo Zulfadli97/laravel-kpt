505963d5cabc5b137bfc0aee4cf254eff3f7dc36
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-10-30 10:09:42 +0800

    add dropdown menu for profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,11 +57,15 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
+                                    <a class="dropdown-item" href="{{ route('my-profile') }}">My Profile</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
